314404a60fb610daa12262c030abc3016263b3a6
Andrew Kuksov <andrew.kuksov@intellectsoft.net>
2017-09-20 13:09:48 +0300

    Add log about bot start up

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import time
+
 import importdir
 from config import *
 
 importdir.do('plugins', globals())
 bot.polling(none_stop=False, interval=0, timeout=20)
+start_date = datetime.datetime.fromtimestamp(int(round(time.time()))).strftime('%b, %d %H:%M')
+print("Bot is running...")
