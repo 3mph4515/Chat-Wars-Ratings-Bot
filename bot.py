f0534e814b0aadb3cd08ae7ad80bcc6f38742b59
Andrew Kuksov <andrew.kuksov@intellectsoft.net>
2017-09-22 00:13:05 +0300

    Remove duplicates cleaning

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import time
 import importdir
 from config import *
 
-remove_duplicates()
 importdir.do('plugins', globals())
 bot.polling(none_stop=False, interval=0, timeout=20)
 start_date = datetime.datetime.fromtimestamp(int(round(time.time()))).strftime('%b, %d %H:%M')
